6734fd80b741f65d5e3186243478154531edaa0c
Sashko Stubailo <sashko@stubailo.com>
2016-02-12 18:04:29 -0800

    Add test about introspecting star wars

--- test/tests.js
+++ test/tests.js
@@ -1,22 +1,11 @@
 import assert from "assert";
-import {version as pkgVersion} from "../package.json";
-import {version as srcVersion, sleep as srcSleep} from "../src/index.js";
-import {version as libVersion, sleep as libSleep} from "../lib/index.js";
+import { introspectStarwars } from './introspectStarwars';
 
-describe("jsnext-skeleton", () => {
-  it("can be imported", () => {
-    assert.strictEqual(pkgVersion, srcVersion);
-    assert.strictEqual(pkgVersion, libVersion);
-  });
+describe("graphql", () => {
+  it("can introspect star wars", async () => {
+    const result = await introspectStarwars();
 
-  it("supports async", async () => {
-    const start = Date.now();
-    await Promise.all([
-      srcSleep(100), srcSleep(100), srcSleep(100),
-      libSleep(100), libSleep(100), libSleep(100)
-    ]).then(hundreds => {
-      assert.ok(hundreds.every(n => n === 100));
-      assert.ok(Date.now() - start < 150);
-    });
+    assert.ok(result.data);
+    assert.ok(result.data.__schema);
   });
 });
